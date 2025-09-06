4adfc62a8ac73c239ff04487e858d96e028e4aea
RalphHontiveros <hontiverosralph00@gmail.com>
2025-09-06 19:41:54 +0800

    update

--- lib/hooks/userBoards.ts
+++ lib/hooks/userBoards.ts
@@ -1,6 +1,16 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { boardDataService } from "../services";
+import { useState } from "react";
+import { Board } from "../supabase/models";
 
 
 export function useBoards() {
+    const {user} = useUser()
+    const {boards, setBoards} = useState<Board[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
 
     async function createBoard(boardData: {
         title: string;
@@ -8,7 +18,18 @@ export function useBoards() {
         color?: string;
     }) {
         
+        if (!user) throw new Error("User not authenticated");
+
+        try{
+            const newBoard = await boardDataService.createBoardWithDefault({
+                ...boardData,
+                userId: user?.id
+            });
+            setBoards((prev) => [...prev, newBoard]);
+        } catch (err) {
+            setError(err instanceof Error ? err.message : "Failed to create baord.");
+        }
     }
 
-    return {createBoard}
+    return { boards, loading, error, createBoard};
 }
